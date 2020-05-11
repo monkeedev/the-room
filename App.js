0413dccce90dd9914e1d03605274572ecefd14ef
Sergey <homevictim19@gmail.com>
2020-05-11 21:19:20 +0300

    2 quests

---
+++ App.js
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2017-present, Viro, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Text,
+  View,
+  StyleSheet,
+  PixelRatio,
+  TouchableHighlight,
+} from 'react-native';
+
+import {
+  ViroVRSceneNavigator,
+  ViroARSceneNavigator
+} from 'react-viro';
+
+/*
+ TODO: Insert your API key below
+ */
+var sharedProps = {
+  apiKey:"API_KEY_HERE",
+}
+
+// Sets the default scene you want for AR and VR
+var InitialVRScene = require('./js/Game');
+
+var UNSET = "UNSET";
+var VR_NAVIGATOR_TYPE = "VR";
+
+// This determines which type of experience to launch in, or UNSET, if the user should
+// be presented with a choice of AR or VR. By default, we offer the user a choice.
+var defaultNavigatorType = UNSET;
+
+export default class ViroSample extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      navigatorType : defaultNavigatorType,
+      sharedProps : sharedProps
+    }
+    this._getVRNavigator = this._getVRNavigator.bind(this);
+    this._exitViro = this._exitViro.bind(this);
+  }
+
+  // Replace this function with the contents of _getVRNavigator() or _getARNavigator()
+  // if you are building a specific type of experience.
+  render() {
+    return this._getVRNavigator();
+  }
+
+  // Returns the ViroSceneNavigator which will start the VR experience
+  _getVRNavigator() {
+    return (
+      <ViroVRSceneNavigator {...this.state.sharedProps}
+        initialScene={{scene: InitialVRScene}} 
+        onExitViro={this._exitViro}
+        // remove for stereo (VR) mode
+        vrModeEnabled={false}
+        />
+    );
+  }
+
+  // This function returns an anonymous/lambda function to be used
+  // by the experience selector buttons
+  _getExperienceButtonOnPress(navigatorType) {
+    return () => {
+      this.setState({
+        navigatorType : navigatorType
+      })
+    }
+  }
+
+  // This function "exits" Viro by setting the navigatorType to UNSET.
+  _exitViro() {
+    this.setState({
+      navigatorType : UNSET
+    })
+  }
+}
+
+module.exports = ViroSample
